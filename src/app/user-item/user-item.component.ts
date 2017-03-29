cb809078afd0b53b11e7f5661588e3f200b6e104
nyinyizin <nyinyizin11@gmail.com>
2017-03-29 18:12:56 +0800

    ngddit initial commit

--- src/app/user-item/user-item.component.ts
+++ src/app/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserItemComponent implements OnInit {
 
-  name:string;
+  @Input() name:string;
 
   constructor() {
-    this.name = 'none';
+
   }
 
   ngOnInit() {
